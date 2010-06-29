bc627ea2924d442a9604cb536180b4b891ade607
David Grudl <david@grudl.com>
2010-06-30 00:20:47 +0200

    improved framework & unit tests directory structure

---
+++ src/Latte/SnippetHelper.php
@@ -0,0 +1,94 @@
+<?php
+
+/**
+ * Nette Framework
+ *
+ * @copyright  Copyright (c) 2004, 2010 David Grudl
+ * @license    http://nette.org/license  Nette license
+ * @link       http://nette.org
+ * @category   Nette
+ * @package    Nette\Templates
+ */
+
+namespace Nette\Templates;
+
+use Nette;
+
+
+
+/**
+ * Control snippet template helper.
+ *
+ * @copyright  Copyright (c) 2004, 2010 David Grudl
+ * @package    Nette\Templates
+ * @deprecated
+ */
+class SnippetHelper extends Nette\Object
+{
+	/** @var bool */
+	public static $outputAllowed = TRUE;
+
+	/** @var string */
+	private $id;
+
+	/** @var string */
+	private $tag;
+
+	/** @var ArrayObject */
+	private $payload;
+
+	/** @var int */
+	private $level;
+
+
+
+	/**
+	 * Starts conditional snippet rendering. Returns SnippetHelper object if snippet was started.
+	 * @param  Control control
+	 * @param  string  snippet name
+	 * @param  string  start element
+	 * @return SnippetHelper
+	 */
+	public static function create(Nette\Application\Control $control, $name = NULL, $tag = 'div')
+	{
+		if (self::$outputAllowed) { // rendering flow or non-AJAX request
+			$obj = new self;
+			$obj->tag = trim($tag, '<>');
+			if ($obj->tag) echo '<', $obj->tag, ' id="', $control->getSnippetId($name), '">';
+			return $obj; // or string?
+
+		} elseif ($control->isControlInvalid($name)) { // start snippet buffering
+			$obj = new self;
+			$obj->id = $control->getSnippetId($name);
+			$obj->payload = $control->getPresenter()->getPayload();
+			ob_start();
+			$obj->level = ob_get_level();
+			self::$outputAllowed = TRUE;
+			return $obj;
+
+		} else {
+			return FALSE;
+		}
+	}
+
+
+
+	/**
+	 * Finishes and saves the snippet.
+	 * @return void
+	 */
+	public function finish()
+	{
+		if ($this->tag !== NULL) { // rendering flow or non-AJAX request
+			if ($this->tag) echo "</$this->tag>";
+
+		} else {  // finish snippet buffering
+			if ($this->level !== ob_get_level()) {
+				throw new \InvalidStateException("Snippet '$this->id' cannot be ended here.");
+			}
+			$this->payload->snippets[$this->id] = ob_get_clean();
+			self::$outputAllowed = FALSE;
+		}
+	}
+
+}
