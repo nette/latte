35cc594622b130b7b324a6a682c7fd25325bd159
David Grudl <david@grudl.com>
2010-11-23 22:21:22 +0100

    All classes and interfaces are in its own file now

---
+++ src/Latte/LatteException.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Nette Framework.
+ *
+ * Copyright (c) 2004, 2010 David Grudl (http://davidgrudl.com)
+ *
+ * This source file is subject to the "Nette license", and/or
+ * GPL license. For more information please see http://nette.org
+ */
+
+namespace Nette\Templates;
+
+use Nette;
+
+
+
+/**
+ * The exception occured during Latte compilation.
+ *
+ * @author     David Grudl
+ */
+class LatteException extends TemplateException
+{
+}
