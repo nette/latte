0ddcaada1c6a937a310f928863c399447687027e
David Grudl <david@grudl.com>
2014-03-20 23:47:21 +0100

    added testing support files

---
+++ tests/bootstrap.php
@@ -0,0 +1,43 @@
+<?php
+
+
+if (@!include __DIR__ . '/../vendor/autoload.php') {
+	echo 'Install Nette Tester using `composer update --dev`';
+	exit(1);
+}
+
+
+// configure environment
+Tester\Environment::setup();
+date_default_timezone_set('Europe/Prague');
+
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
+@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
+Tester\Helpers::purge(TEMP_DIR);
+
+
+function test(\Closure $function)
+{
+	$function();
+}
+
+
+class Notes
+{
+	static public $notes = array();
+
+	public static function add($message)
+	{
+		self::$notes[] = $message;
+	}
+
+	public static function fetch()
+	{
+		$res = self::$notes;
+		self::$notes = array();
+		return $res;
+	}
+
+}
