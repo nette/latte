aa9abd6c8abd7c6c18940b950954faa539cc1bed
David Grudl <david@grudl.com>
2014-03-21 05:33:00 +0100

    added readme.md

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+// The Nette Tester command-line runner can be
+// invoked through the command: ../vendor/bin/tester .
 
 if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer update --dev`';
