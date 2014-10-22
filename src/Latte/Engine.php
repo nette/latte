b7380a07b1273145629a59f09fe9ce254f58122e
chemiX <iam@chemix.cz>
2014-10-22 18:43:14 +0200

    Latte\Engine: silent mkdir in getCacheFile
    fix #39

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -194,7 +194,10 @@ class Engine extends Object
 		if (!$this->tempDirectory) {
 			throw new \RuntimeException('Set path to temporary directory using setTempDirectory().');
 		} elseif (!is_dir($this->tempDirectory)) {
-			mkdir($this->tempDirectory);
+			@mkdir($this->tempDirectory); // High concurrency
+			if (!is_dir($this->tempDirectory)) {
+				throw new \RuntimeException("Temporary directory cannot be created. Check access rights");
+			}
 		}
 		$file = md5($name);
 		if (preg_match('#\b\w.{10,50}$#', $name, $m)) {
