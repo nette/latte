59226ef5080d7a10fb9d80817c24ef284cef246d
David Grudl <david@grudl.com>
2015-10-05 15:30:23 +0200

    used https

--- src/Latte/Runtime/Html.php
+++ src/Latte/Runtime/Html.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * This file is part of the Latte (http://latte.nette.org)
- * Copyright (c) 2008 David Grudl (http://davidgrudl.com)
+ * This file is part of the Latte (https://latte.nette.org)
+ * Copyright (c) 2008 David Grudl (https://davidgrudl.com)
  */
 
 namespace Latte\Runtime;
