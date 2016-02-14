74657184f9076b11d60cef84099655ba00382666
rundef <bash88@gmail.com>
2016-02-14 13:27:50 -0500

    Improve code + add unit tests

--- src/Latte/Runtime/Filters.php
+++ src/Latte/Runtime/Filters.php
@@ -366,9 +366,13 @@ class Filters
 	 */
 	public static function length($s)
 	{
-		return is_string($s) ?
-			strlen(utf8_decode($s)): // fastest way
-			count($s);
+	    if (is_array($s) || $s instanceof \Countable) {
+	        return count($s);
+	    } elseif ($s instanceof \Traversable) {
+	        return iterator_count($s);
+	    } else {
+	        return strlen(utf8_decode($s)); // fastest way
+	    }
 	}
 
 
