37507c53fc195e26bfd1fcf50bd0b940dc18f21c
David Grudl <david@grudl.com>
2016-05-05 10:34:53 +0200

    FileLoader: can restrict that file is in base directory

--- src/Latte/Loaders/FileLoader.php
+++ src/Latte/Loaders/FileLoader.php
@@ -17,13 +17,26 @@ class FileLoader implements Latte\ILoader
 {
 	use Latte\Strict;
 
+	/** @var string|NULL */
+	private $baseDir;
+
+
+	public function __construct($baseDir = NULL)
+	{
+		$this->baseDir = $baseDir ? $this->normalizePath("$baseDir/") : NULL;
+	}
+
 	/**
 	 * Returns template source code.
 	 * @return string
 	 */
 	public function getContent($file)
 	{
-		if (!is_file($file)) {
+		$file = $this->baseDir . $file;
+		if ($this->baseDir && strncmp($this->normalizePath($file), $this->baseDir, strlen($this->baseDir))) {
+			throw new \RuntimeException("Template '$file' is not within the allowed path '$this->baseDir'.");
+
+		} elseif (!is_file($file)) {
 			throw new \RuntimeException("Missing template file '$file'.");
 
 		} elseif ($this->isExpired($file, time())) {
@@ -40,7 +53,7 @@ class FileLoader implements Latte\ILoader
 	 */
 	public function isExpired($file, $time)
 	{
-		return @filemtime($file) > $time; // @ - stat may fail
+		return @filemtime($this->baseDir . $file) > $time; // @ - stat may fail
 	}
 
 
@@ -50,8 +63,9 @@ class FileLoader implements Latte\ILoader
 	 */
 	public function getReferredName($file, $referringFile)
 	{
-		if (!preg_match('#/|\\\\|[a-z][a-z0-9+.-]*:#iA', $file)) {
-			$file = dirname($referringFile) . '/' . $file;
+		if ($this->baseDir || !preg_match('#/|\\\\|[a-z][a-z0-9+.-]*:#iA', $file)) {
+			$file = dirname($this->baseDir . $referringFile) . '/' . $file;
+			$file = substr($file, strlen($this->baseDir));
 		}
 		return $file;
 	}
@@ -63,7 +77,24 @@ class FileLoader implements Latte\ILoader
 	 */
 	public function getUniqueId($file)
 	{
-		return $file;
+		return $this->baseDir . $file;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	private static function normalizePath($path)
+	{
+		$res = [];
+		foreach (explode('/', strtr($path, '\\', '/')) as $part) {
+			if ($part === '..') {
+				array_pop($res);
+			} elseif ($part !== '.') {
+				$res[] = $part;
+			}
+		}
+		return implode(DIRECTORY_SEPARATOR, $res);
 	}
 
 }
--- tests/Latte/Loaders.FileLoader.phpt
+++ tests/Latte/Loaders.FileLoader.phpt
@@ -25,3 +25,18 @@ Assert::exception(function () {
 	$loader = new FileLoader;
 	$loader->getContent('unknown');
 }, 'RuntimeException', "Missing template file 'unknown'.");
+
+
+$loader = new FileLoader(dirname(__DIR__));
+Assert::same(file_get_contents(__FILE__), $loader->getContent('Latte/' . basename(__FILE__)));
+
+Assert::exception(function () use ($loader) {
+	$loader->getContent('Latte/.././../file');
+}, 'RuntimeException', "Template '%a%Latte/.././../file' is not within the allowed path '%a%'.");
+
+Assert::false($loader->isExpired('Latte/' . basename(__FILE__), filemtime(__FILE__) + 1));
+Assert::true($loader->isExpired('Latte/' . basename(__FILE__), filemtime(__FILE__) - 1));
+
+Assert::same('Latte/' . basename(__FILE__), $loader->getReferredName(basename(__FILE__), 'Latte/file'));
+Assert::same('Latte', $loader->getReferredName('Latte', 'file'));
+Assert::same('../tests', $loader->getReferredName('../tests', 'file'));
