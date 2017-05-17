b6163f116f5fa75d780eae13c618ab98984dc986
Andrej Rypo <dakujem@users.noreply.github.com>
2017-05-17 13:30:19 +0200

    macro n:attr expands array (#158)
    When the first and only argument to n:attr macro is an array, expand it.

--- src/Latte/Macros/CoreMacros.php
+++ src/Latte/Macros/CoreMacros.php
@@ -395,7 +395,7 @@ class CoreMacros extends MacroSet
 	 */
 	public function macroAttr(MacroNode $node, PhpWriter $writer)
 	{
-		return $writer->write('echo LR\Filters::htmlAttributes(%node.array);');
+		return $writer->write('$_tmp = %node.array; echo LR\Filters::htmlAttributes(isset($_tmp[0]) && is_array($_tmp[0]) ? $_tmp[0] : $_tmp);');
 	}
 
 
