23487e23103ce5f4760d9dbc048ee9d5357b485e
David Grudl <david@grudl.com>
2019-07-16 16:47:16 +0200

    opened 3.0-dev

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -17,8 +17,8 @@ class Engine
 {
 	use Strict;
 
-	public const VERSION = '2.11.4';
-	public const VERSION_ID = 21104;
+	public const VERSION = '3.0.0-dev';
+	public const VERSION_ID = 30000;
 
 	/** Content types */
 	public const
