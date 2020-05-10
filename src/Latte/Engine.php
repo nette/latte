a96a004eb2ef5d7c98c0cd124f8dd62cb854e6b1
David Grudl <david@grudl.com>
2020-05-10 11:18:03 +0200

    opened 2.9-dev

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -17,8 +17,8 @@ class Engine
 {
 	use Strict;
 
-	public const VERSION = '2.8.3';
-	public const VERSION_ID = 20803;
+	public const VERSION = '2.9.0-dev';
+	public const VERSION_ID = 20900;
 
 	/** Content types */
 	public const
