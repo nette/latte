321f17d07d1091af8b5001d1c3cbd178b6b89b55
David Grudl <david@grudl.com>
2020-11-03 21:58:28 +0100

    Released version 2.8.3

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -17,8 +17,8 @@ class Engine
 {
 	use Strict;
 
-	public const VERSION = '2.8.2';
-	public const VERSION_ID = 20802;
+	public const VERSION = '2.8.3';
+	public const VERSION_ID = 20803;
 
 	/** Content types */
 	public const
