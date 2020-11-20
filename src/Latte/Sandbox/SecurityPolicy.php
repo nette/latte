8b7024fe02ff5f268ee149c7a2e360851e74fca5
David Grudl <david@grudl.com>
2020-11-20 23:37:45 +0100

    SecurityPolicy: added new tags & filters

--- src/Latte/Sandbox/SecurityPolicy.php
+++ src/Latte/Sandbox/SecurityPolicy.php
@@ -51,15 +51,15 @@ class SecurityPolicy implements Latte\Policy
 		// php (but 'do' is allowed), sandbox, snippet, snippetArea, templatePrint, varPrint, embed
 		$policy->allowMacros([
 			'_', '=', 'attr', 'block', 'breakIf', 'capture', 'case', 'class', 'continueIf', 'default',
-			'define', 'do', 'else', 'elseif', 'elseifset', 'first', 'for', 'foreach', 'if', 'ifcontent',
-			'ifset', 'l', 'last', 'r', 'sep', 'spaceless', 'switch', 'templateType', 'var', 'varType', 'while',
+			'define', 'do', 'else', 'elseif', 'elseifset', 'first', 'for', 'foreach', 'if', 'ifchanged', 'ifcontent',
+			'ifset', 'l', 'last', 'r', 'rollback', 'sep', 'skipIf', 'spaceless', 'switch', 'templateType', 'try', 'var', 'varType', 'while',
 		]);
 
 		// does not include: dataStream, noEscape, noCheck
 		$policy->allowFilters([
-			'batch', 'breakLines', 'bytes', 'capitalize', 'date', 'escapeCss', 'escapeHtml', 'escapeHtmlComment', 'escapeICal',
+			'batch', 'breakLines', 'bytes', 'capitalize', 'clamp', 'date', 'escapeCss', 'escapeHtml', 'escapeHtmlComment', 'escapeICal',
 			'escapeJs', 'escapeUrl', 'escapeXml', 'firstUpper', 'checkUrl', 'implode', 'indent', 'length', 'lower', 'number',
-			'padLeft', 'padRight', 'repeat', 'replace', 'replaceRe', 'reverse', 'strip', 'stripHtml', 'stripTags', 'substr',
+			'padLeft', 'padRight', 'repeat', 'replace', 'replaceRe', 'reverse', 'sort', 'strip', 'stripHtml', 'stripTags', 'substr',
 			'trim', 'truncate', 'upper', 'webalize',
 		]);
 
