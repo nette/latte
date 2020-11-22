18d3078408ff47652ab75afe1347f1758cbbee72
David Grudl <david@grudl.com>
2020-11-23 00:40:51 +0100

    RootNode

---
+++ src/Latte/Compiler/RootNode.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Latte (https://latte.nette.org)
+ * Copyright (c) 2008 David Grudl (https://davidgrudl.com)
+ */
+
+declare(strict_types=1);
+
+namespace Latte\Compiler;
+
+
+
+class RootNode implements Node
+{
+	/** @var Node[] */
+	public $children = [];
+}
