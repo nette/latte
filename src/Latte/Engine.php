f66806cb6dff34ae536348524bb20965f74766fe
David Grudl <david@grudl.com>
2020-12-17 00:16:28 +0100

    Released version 2.9.2

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -17,8 +17,8 @@ class Engine
 {
 	use Strict;
 
-	public const VERSION = '2.9.1';
-	public const VERSION_ID = 20901;
+	public const VERSION = '2.9.2';
+	public const VERSION_ID = 20902;
 
 	/** Content types */
 	public const
