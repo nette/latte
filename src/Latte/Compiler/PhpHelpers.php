dce724e8e91036d71e3000d82743719b59c9965e
David Grudl <david@grudl.com>
2022-03-21 23:04:21 +0100

    PhpHelpers::reformatCode() simplified, removed support for inline HTML

--- src/Latte/Compiler/PhpHelpers.php
+++ src/Latte/Compiler/PhpHelpers.php
@@ -23,60 +23,26 @@ final class PhpHelpers
 	 */
 	public static function reformatCode(string $source): string
 	{
-		$res = $php = '';
+		$res = '';
 		$lastChar = ';';
 		$tokens = new \ArrayIterator(token_get_all($source));
-		$level = $openLevel = 0;
-		$lineLength = 100;
-		$specialBrace = false;
+		$level = 0;
 
 		foreach ($tokens as $n => $token) {
 			$next = $tokens[$n + 1] ?? [null, ''];
 
 			if (is_array($token)) {
-				[$name, $token] = ($tmp = $token);
-				if ($name === T_INLINE_HTML) {
-					$res .= $token;
-
-				} elseif ($name === T_OPEN_TAG) {
-					$openLevel = $level;
-
-				} elseif ($name === T_CLOSE_TAG) {
-					if ($next[0] === T_OPEN_TAG) { // remove ?)<?php
-						if (!strspn($lastChar, ';{:/' . ($specialBrace ? '' : '}'))) {
-							$php = rtrim($php) . ($lastChar = ';') . "\n" . str_repeat("\t", $level);
-						} elseif (substr($next[1], -1) === "\n") {
-							$php .= "\n" . str_repeat("\t", $level);
-						}
-
-						$tokens->next();
-
-					} else {
-						if (trim($php) !== '' || substr($res, -1) === '<') { // skip <?php ?) but preserve <<?php
-							$inline = !str_contains($php, "\n") && strlen($res) - strrpos($res, "\n") < $lineLength;
-							$res .= '<?php' . ($inline ? ' ' : "\n" . str_repeat("\t", $openLevel));
-							if (!str_contains($next[1], "\n")) {
-								$token = rtrim($token, "\n");
-							} else {
-								$php = rtrim($php, "\t");
-							}
-
-							$res .= $php . $token;
-						}
-
-						$php = '';
-						$lastChar = ';';
-					}
-				} elseif ($name === T_ELSE || $name === T_ELSEIF) {
+				[$name, $token] = $token;
+				if ($name === T_ELSE || $name === T_ELSEIF) {
 					if ($next === ':' && $lastChar === '}') {
-						$php .= ';'; // semicolon needed in if(): ... if() ... else:
+						$res .= ';'; // semicolon needed in if(): ... if() ... else:
 					}
 
 					$lastChar = '';
-					$php .= $token;
+					$res .= $token;
 
 				} elseif ($name === T_DOC_COMMENT || $name === T_COMMENT) {
-					$php .= preg_replace("#\n[ \t]*+(?!\n)#", "\n" . str_repeat("\t", $level), $token);
+					$res .= preg_replace("#\n[ \t]*+(?!\n)#", "\n" . str_repeat("\t", $level), $token);
 
 				} elseif ($name === T_WHITESPACE) {
 					$prev = $tokens[$n - 1];
@@ -89,11 +55,17 @@ final class PhpHelpers
 						$token = '';
 					}
 
-					$php .= $token;
+					$res .= $token;
 
 				} elseif ($name === T_OBJECT_OPERATOR) {
 					$lastChar = '->';
-					$php .= $token;
+					$res .= $token;
+
+				} elseif ($name === T_OPEN_TAG) {
+					$res .= "<?php\n";
+
+				} elseif ($name === T_CLOSE_TAG) {
+					throw new \LogicException('Unexpected token');
 
 				} else {
 					if (in_array($name, [T_CURLY_OPEN, T_DOLLAR_OPEN_CURLY_BRACES], true)) {
@@ -101,32 +73,24 @@ final class PhpHelpers
 					}
 
 					$lastChar = '';
-					$php .= $token;
+					$res .= $token;
 				}
 			} else {
 				if ($token === '{' || $token === '[') {
 					$level++;
-					if ($lastChar === '->' || $lastChar === '$') {
-						$specialBrace = true;
-					}
 				} elseif ($token === '}' || $token === ']') {
 					$level--;
-					$php .= "\x08";
+					$res .= "\x08";
 
 				} elseif ($token === ';') {
-					$specialBrace = false;
 					if ($next[0] !== T_WHITESPACE) {
 						$token .= "\n" . str_repeat("\t", $level); // indent last line
 					}
 				}
 
 				$lastChar = $token;
-				$php .= $token;
+				$res .= $token;
 			}
-		}
-
-		if ($php) {
-			$res .= "<?php\n" . str_repeat("\t", $openLevel) . $php;
 		}
 
 		$res = str_replace(["\t\x08", "\x08"], '', $res);
