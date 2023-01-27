0eb8006219a2008a246c4325f678e049a8f11bf4
David Grudl <david@grudl.com>
2023-01-27 18:02:22 +0100

    NameNode: updated list of keywords, enumerated magic constants

--- src/Latte/Compiler/Nodes/Php/NameNode.php
+++ src/Latte/Compiler/Nodes/Php/NameNode.php
@@ -52,17 +52,17 @@ class NameNode extends Node
 	public function isKeyword(): bool
 	{
 		static $keywords;
-		$keywords ??= array_flip([
-			'include', 'include_once', 'eval', 'require', 'require_once', 'or', 'xor', 'and',
-			'instanceof', 'new', 'clone', 'exit', 'if', 'elseif', 'else', 'endif', 'echo', 'do', 'while',
-			'endwhile', 'for', 'endfor', 'foreach', 'endforeach', 'declare', 'enddeclare', 'as', 'try', 'catch',
-			'finally', 'throw', 'use', 'insteadof', 'global', 'var', 'unset', 'isset', 'empty', 'continue', 'goto',
-			'function', 'const', 'return', 'print', 'yield', 'list', 'switch', 'endswitch', 'case', 'default',
-			'break', 'array', 'callable', 'extends', 'implements', 'namespace', 'trait', 'interface', 'class',
-			'static', 'abstract', 'final', 'private', 'protected', 'public', 'fn', 'match', 'self', 'parent',
+		$keywords ??= array_flip([ // https://www.php.net/manual/en/reserved.keywords.php
+			'__halt_compiler', '__class__', '__dir__', '__file__', '__function__', '__line__', '__method__', '__namespace__', '__trait__',
+			'abstract', 'and', 'array', 'as', 'break', 'callable', 'case', 'catch', 'class', 'clone', 'const', 'continue', 'declare',
+			'default', 'die', 'do', 'echo', 'else', 'elseif', 'empty', 'enddeclare', 'endfor', 'endforeach', 'endif', 'endswitch',
+			'endwhile', 'eval', 'exit', 'extends', 'final', 'finally', 'fn', 'for', 'foreach', 'function', 'global', 'goto', 'if',
+			'implements', 'include', 'include_once', 'instanceof', 'insteadof', 'interface', 'isset', 'list', 'match', 'namespace',
+			'new', 'or', 'print', 'private', 'protected', 'public', 'readonly', 'require', 'require_once', 'return', 'static',
+			'switch', 'throw', 'trait', 'try', 'unset', 'use', 'var', 'while', 'xor', 'yield',
+			'parent', 'self', 'mixed', 'void', 'enum', // extra
 		]);
-		return count($this->parts) === 1
-			&& (isset($keywords[strtolower($this->parts[0])]) || str_starts_with($this->parts[0], '__'));
+		return count($this->parts) === 1 && isset($keywords[strtolower($this->parts[0])]);
 	}
 
 
