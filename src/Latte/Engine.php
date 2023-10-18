0ac0843a459790d471821f6a82f5d13db831a0d3
David Grudl <david@grudl.com>
2023-10-18 19:16:11 +0200

    Released version 2.11.7

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -17,8 +17,8 @@ class Engine
 {
 	use Strict;
 
-	public const VERSION = '2.11.6';
-	public const VERSION_ID = 21106;
+	public const VERSION = '2.11.7';
+	public const VERSION_ID = 21107;
 
 	/** Content types */
 	public const
