11915defd927239e584e4e9b7a8a596aec31cec9
David Grudl <david@grudl.com>
2024-02-03 18:26:15 +0100

    PHP 8.4 compatibility

--- src/Latte/Helpers.php
+++ src/Latte/Helpers.php
@@ -46,7 +46,9 @@ class Helpers
 	public static function toReflection($callable): \ReflectionFunctionAbstract
 	{
 		if (is_string($callable) && strpos($callable, '::')) {
-			return new \ReflectionMethod($callable);
+			return PHP_VERSION_ID < 80300
+				? new \ReflectionMethod($callable)
+				: \ReflectionMethod::createFromMethodName($callable);
 		} elseif (is_array($callable)) {
 			return new \ReflectionMethod($callable[0], $callable[1]);
 		} elseif (is_object($callable) && !$callable instanceof \Closure) {
