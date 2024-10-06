4db7a5502f8cef02fffa84fc9c34a635d9c79d4d
David Grudl <david@grudl.com>
2024-10-07 01:34:23 +0200

    Released version 3.0.20

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -17,8 +17,8 @@ use Latte\Compiler\Nodes\TemplateNode;
  */
 class Engine
 {
-	public const Version = '3.0.18';
-	public const VersionId = 30018;
+	public const Version = '3.0.20';
+	public const VersionId = 30020;
 
 	/** @deprecated use Engine::Version */
 	public const
