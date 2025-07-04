37e0b037dee4e9663f2318b797e811541e69fbcb
David Grudl <david@grudl.com>
2025-07-04 03:47:26 +0200

    ElementNode refactoring, added TagNode (BC break)

--- tests/common/expected/Compiler.variable.tags.php
+++ tests/common/expected/Compiler.variable.tags.php
@@ -7,30 +7,36 @@ final class Template%a% extends Latte\Runtime\Template
 	{
 %A%
 		echo "\n";
-		$ʟ_tag[0] = '';
+		$ʟ_tag = '';
 		$ʟ_tmp = LR\HtmlHelpers::validateTagChange($tag, '');
-		$ʟ_tag[0] = '</' . $ʟ_tmp . '>' . $ʟ_tag[0];
-		echo '<', $ʟ_tmp /* line %d% */;
-		echo '>...';
-		echo $ʟ_tag[0];
+		$ʟ_tag = '</' . $ʟ_tmp . '>' . $ʟ_tag;
+		echo '<', $ʟ_tmp /* line 3 */;
+		echo '>';
+		$ʟ_tags[0] = $ʟ_tag;
+		echo '...';
+		echo $ʟ_tags[0];
 		echo '
 
 ';
-		$ʟ_tag[1] = '';
+		$ʟ_tag = '';
 		$ʟ_tmp = LR\HtmlHelpers::validateTagChange($ns . ':' . $tag, '');
-		$ʟ_tag[1] = '</' . $ʟ_tmp . '>' . $ʟ_tag[1];
-		echo '<', $ʟ_tmp /* line %d% */;
-		echo '>...';
-		echo $ʟ_tag[1];
+		$ʟ_tag = '</' . $ʟ_tmp . '>' . $ʟ_tag;
+		echo '<', $ʟ_tmp /* line 5 */;
+		echo '>';
+		$ʟ_tags[1] = $ʟ_tag;
+		echo '...';
+		echo $ʟ_tags[1];
 		echo '
 
 ';
-		$ʟ_tag[2] = '';
+		$ʟ_tag = '';
 		$ʟ_tmp = LR\HtmlHelpers::validateTagChange('h' . 1, '');
-		$ʟ_tag[2] = '</' . $ʟ_tmp . '>' . $ʟ_tag[2];
-		echo '<', $ʟ_tmp /* line %d% */;
-		echo '>...';
-		echo $ʟ_tag[2];
+		$ʟ_tag = '</' . $ʟ_tmp . '>' . $ʟ_tag;
+		echo '<', $ʟ_tmp /* line 7 */;
+		echo '>';
+		$ʟ_tags[2] = $ʟ_tag;
+		echo '...';
+		echo $ʟ_tags[2];
 	}
 %A%
 }
