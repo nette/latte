45ce5968b40c35a256a263f69f6664708dc56c09
David Grudl <david@grudl.com>
2025-08-06 12:40:24 +0200

    undefined unsafe operator ??-> is deprecated (BC break)

--- tests/sandbox/expected/code.optional-chaining.php
+++ tests/sandbox/expected/code.optional-chaining.php
@@ -3,13 +3,9 @@
 		echo 'optional chaining
 
 ';
-		echo LR\Filters::escapeHtmlText($this->global->sandbox->prop($obj, 'prop')?->prop) /* line %d% */;
+		echo LR\Filters::escapeHtmlText($this->global->sandbox->prop($obj, 'prop')?->prop) /* line 3 */;
 		echo "\n";
-		echo LR\Filters::escapeHtmlText($this->global->sandbox->prop($obj ?? null, 'prop')?->prop) /* line %d% */;
-		echo "\n";
-		echo LR\Filters::escapeHtmlText($this->global->sandbox->callMethod($obj, 'bar', [], true)) /* line %d% */;
-		echo "\n";
-		echo LR\Filters::escapeHtmlText($this->global->sandbox->callMethod($obj ?? null, 'bar', [], true)) /* line %d% */;
+		echo LR\Filters::escapeHtmlText($this->global->sandbox->callMethod($obj, 'bar', [], true)) /* line 4 */;
 		echo '
 -';
 %A%
