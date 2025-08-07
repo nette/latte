f4aa12db4161a41879886c4d7adcb1d89cab94f7
David Grudl <david@grudl.com>
2025-08-08 01:01:43 +0200

    Escaper: supports converting HTML => TEXT

--- src/Latte/Compiler/Escaper.php
+++ src/Latte/Compiler/Escaper.php
@@ -61,6 +61,7 @@ final class Escaper
 			self::HtmlComment => 'escapeHtmlComment',
 		],
 		self::HtmlText => [
+			self::Text => 'convertHtmlToText',
 			self::HtmlAttribute => 'convertHtmlToHtmlAttr',
 			self::HtmlAttribute . '/' . self::JavaScript => 'convertHtmlToHtmlAttr',
 			self::HtmlAttribute . '/' . self::Css => 'convertHtmlToHtmlAttr',
