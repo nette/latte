63ced0e812206e3c1829493050e20e874119bf60
David Grudl <david@grudl.com>
2025-12-23 21:12:38 +0100

    added |column filter

--- src/Latte/Essential/CoreExtension.php
+++ src/Latte/Essential/CoreExtension.php
@@ -125,6 +125,7 @@ final class CoreExtension extends Latte\Extension
 			'ceil' => $this->filters->ceil(...),
 			'checkUrl' => $this->filters->checkUrl(...),
 			'clamp' => $this->filters->clamp(...),
+			'column' => $this->filters->column(...),
 			'dataStream' => $this->filters->dataStream(...),
 			'datastream' => $this->filters->dataStream(...),
 			'date' => $this->filters->date(...),
