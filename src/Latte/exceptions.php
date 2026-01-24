4cf9c5fc44208643883e5504d74548de35514659
David Grudl <david@grudl.com>
2026-01-24 04:00:51 +0100

    merged PositionAwareException into exceptions.php

--- src/Latte/exceptions.php
+++ src/Latte/exceptions.php
@@ -17,6 +17,43 @@ interface Exception
 
 
 /**
+ * @internal
+ */
+trait PositionAwareException
+{
+	public ?string $sourceCode = null;
+	public ?string $sourceName = null;
+	public ?Compiler\Position $position = null;
+	private string $origMessage;
+
+
+	public function setSource(string $code, ?string $name = null): self
+	{
+		$this->sourceCode = $code;
+		$this->sourceName = $name;
+		$this->generateMessage();
+		return $this;
+	}
+
+
+	private function generateMessage(): void
+	{
+		$this->origMessage ??= $this->message;
+		$info = [];
+		if ($this->sourceName && @is_file($this->sourceName)) { // @ - may trigger error
+			$info[] = "in '" . str_replace(dirname($this->sourceName, 2), '...', $this->sourceName) . "'";
+		}
+		if ($this->position) {
+			$info[] = $this->position;
+		}
+		$this->message = $info
+			? rtrim($this->origMessage, '.') . ' (' . implode(' ', $info) . ')'
+			: $this->origMessage;
+	}
+}
+
+
+/**
  * Template compilation failed.
  */
 class CompileException extends \Exception implements Exception
