18eee40e3c885e8f14aad618f810314ebac1fe3a
David Grudl <david@grudl.com>
2026-03-22 23:57:08 +0100

    Released version 3.1.4

--- src/Latte/Engine.php
+++ src/Latte/Engine.php
@@ -16,8 +16,8 @@ use function array_map, array_merge, class_exists, extension_loaded, get_debug_t
  */
 class Engine
 {
-	public const Version = '3.1.3';
-	public const VersionId = 30103;
+	public const Version = '3.1.4';
+	public const VersionId = 30104;
 
 	/** @deprecated use Engine::Version */
 	public const
